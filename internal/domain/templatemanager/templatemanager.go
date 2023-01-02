1e836e3e434e7911a61e84002e32ca4d1d8a29f7
prot1vogas <lakeev.s.u@gmail.com>
2023-01-02 05:41:00 +0400

    error messages fixes

--- internal/domain/templatemanager/templatemanager.go
+++ internal/domain/templatemanager/templatemanager.go
@@ -2,7 +2,9 @@ package templatemanager
 
 import (
 	"context"
+	"errors"
 
+	"github.com/LakeevSergey/mailer/internal/domain"
 	"github.com/LakeevSergey/mailer/internal/domain/entity"
 	"github.com/LakeevSergey/mailer/internal/domain/templatemanager/dto"
 )
@@ -18,21 +20,56 @@ func NewTemplateManager(storager TemplateStorager) *TemplateManager {
 }
 
 func (m *TemplateManager) Get(ctx context.Context, id int64) (entity.Template, error) {
-	return m.storager.Get(ctx, id)
+	template, err := m.storager.Get(ctx, id)
+	if errors.Is(err, domain.ErrorEntityNotFound) {
+		return entity.Template{}, domain.ErrorTemplateNotFound
+	} else if err != nil {
+		return entity.Template{}, err
+	}
+
+	return template, nil
 }
 
 func (m *TemplateManager) Search(ctx context.Context, dto dto.Search) (templates []entity.Template, total int, err error) {
-	return m.storager.Search(ctx, dto)
+	templates, total, err = m.storager.Search(ctx, dto)
+	if err != nil {
+		return []entity.Template{}, 0, err
+	}
+
+	return templates, total, err
 }
 
 func (m *TemplateManager) Add(ctx context.Context, dto dto.Add) (entity.Template, error) {
-	return m.storager.Add(ctx, dto)
+	template, err := m.storager.Add(ctx, dto)
+	if errors.Is(err, domain.ErrorDuplicate) {
+		return entity.Template{}, domain.ErrorTemplateCodeDuplicate
+	} else if err != nil {
+		return entity.Template{}, err
+	}
+
+	return template, nil
 }
 
 func (m *TemplateManager) Update(ctx context.Context, id int64, dto dto.Update) (entity.Template, error) {
-	return m.storager.Update(ctx, id, dto)
+	template, err := m.storager.Update(ctx, id, dto)
+	if errors.Is(err, domain.ErrorDuplicate) {
+		return entity.Template{}, domain.ErrorTemplateCodeDuplicate
+	} else if errors.Is(err, domain.ErrorEntityNotFound) {
+		return entity.Template{}, domain.ErrorTemplateNotFound
+	} else if err != nil {
+		return entity.Template{}, err
+	}
+
+	return template, nil
 }
 
 func (m *TemplateManager) Delete(ctx context.Context, id int64) error {
-	return m.storager.Delete(ctx, id)
+	err := m.storager.Delete(ctx, id)
+	if errors.Is(err, domain.ErrorEntityNotFound) {
+		return domain.ErrorTemplateNotFound
+	} else if err != nil {
+		return err
+	}
+
+	return nil
 }
