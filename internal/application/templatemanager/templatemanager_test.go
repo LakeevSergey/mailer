bd835adf9b650a814406a32b224e6c1862b87f76
prot1vogas <lakeev.s.u@gmail.com>
2023-01-09 16:31:23 +0400

    template manager get test

---
+++ internal/application/templatemanager/templatemanager_test.go
@@ -0,0 +1,64 @@
+package templatemanager
+
+import (
+	"context"
+	"testing"
+
+	"github.com/LakeevSergey/mailer/internal/domain"
+	"github.com/LakeevSergey/mailer/internal/domain/entity"
+	"github.com/LakeevSergey/mailer/internal/infrastructure/storager/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTemplateManager_Get(t *testing.T) {
+	ctx := context.Background()
+
+	type args struct {
+		ctx context.Context
+		id  int64
+	}
+	tests := []struct {
+		name    string
+		m       *TemplateManager
+		args    args
+		want    entity.Template
+		wantErr error
+	}{
+		{
+			name: "simple test",
+			m: &TemplateManager{
+				storager: mock.NewMockStorager(ctx),
+			},
+			args: args{
+				ctx,
+				1,
+			},
+			want:    mock.ActiveTemplate,
+			wantErr: nil,
+		},
+		{
+			name: "simple test",
+			m: &TemplateManager{
+				storager: mock.NewMockStorager(ctx),
+			},
+			args: args{
+				ctx,
+				3,
+			},
+			want:    entity.Template{},
+			wantErr: domain.ErrorTemplateNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.m.Get(tt.args.ctx, tt.args.id)
+			assert.Equal(t, tt.want, got)
+			if tt.wantErr != nil {
+				assert.ErrorIs(t, tt.wantErr, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+		})
+	}
+}
--- internal/infrastructure/storager/mock/template.go
+++ internal/infrastructure/storager/mock/template.go
@@ -2,9 +2,10 @@ package mock
 
 import (
 	"context"
-	"errors"
 
 	"github.com/LakeevSergey/mailer/internal/domain/entity"
+	"github.com/LakeevSergey/mailer/internal/domain/storager"
+	"github.com/LakeevSergey/mailer/internal/domain/templatemanager/dto"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -26,6 +27,38 @@ var NotActiveTemplate = entity.Template{
 	Title:  "Not active template title, {{ name }}",
 }
 
+var AddSuccess = dto.Add{
+	Active: true,
+	Code:   "new_template",
+	Name:   "New template",
+	Body:   "New active template body, {{ name }}",
+	Title:  "New active template title, {{ name }}",
+}
+
+var AddDublicateError = dto.Add{
+	Active: true,
+	Code:   "active_template",
+	Name:   "New template",
+	Body:   "New active template body, {{ name }}",
+	Title:  "New active template title, {{ name }}",
+}
+
+var UpdateSuccess = dto.Update{
+	Active: true,
+	Code:   "new_template",
+	Name:   "New template",
+	Body:   "New active template body, {{ name }}",
+	Title:  "New active template title, {{ name }}",
+}
+
+var UpdateDublicateError = dto.Update{
+	Active: true,
+	Code:   "active_template",
+	Name:   "New template",
+	Body:   "New active template body, {{ name }}",
+	Title:  "New active template title, {{ name }}",
+}
+
 type MockTemplateStorager struct {
 	mock.Mock
 }
@@ -35,11 +68,78 @@ func NewMockStorager(ctx context.Context) *MockTemplateStorager {
 
 	mockStorager.On("GetByCode", ctx, "active_template").Return(ActiveTemplate, nil)
 	mockStorager.On("GetByCode", ctx, "not_active_template").Return(NotActiveTemplate, nil)
-	mockStorager.On("GetByCode", ctx, "not_existent_template").Return(entity.Template{}, errors.New("entity not found"))
+	mockStorager.On("GetByCode", ctx, "not_existent_template").Return(entity.Template{}, storager.ErrorEntityNotFound)
 
-	mockStorager.On("Get", ctx, 1).Return(ActiveTemplate, nil)
-	mockStorager.On("Get", ctx, 2).Return(NotActiveTemplate, nil)
-	mockStorager.On("Get", ctx, 3).Return(entity.Template{}, errors.New("entity not found"))
+	mockStorager.On("Get", ctx, int64(1)).Return(ActiveTemplate, nil)
+	mockStorager.On("Get", ctx, int64(2)).Return(NotActiveTemplate, nil)
+	mockStorager.On("Get", ctx, int64(3)).Return(entity.Template{}, storager.ErrorEntityNotFound)
+
+	mockStorager.On("Search", ctx, dto.Search{Limit: 10, Offset: 0}).Return(
+		[]entity.Template{
+			ActiveTemplate,
+			NotActiveTemplate,
+		},
+		2,
+		nil,
+	)
+	mockStorager.On("Search", ctx, dto.Search{Limit: 10, Offset: 10}).Return(
+		[]entity.Template{},
+		2,
+		nil,
+	)
+
+	mockStorager.On("Search", ctx, dto.Search{Limit: 10, Offset: 10}).Return(
+		[]entity.Template{},
+		2,
+		nil,
+	)
+
+	mockStorager.On("Add", ctx, AddSuccess).Return(
+		entity.Template{
+			Id:     3,
+			Active: AddSuccess.Active,
+			Code:   AddSuccess.Code,
+			Name:   AddSuccess.Name,
+			Body:   AddSuccess.Body,
+			Title:  AddSuccess.Title,
+		},
+		nil,
+	)
+
+	mockStorager.On("Add", ctx, AddDublicateError).Return(
+		entity.Template{},
+		storager.ErrorDuplicate,
+	)
+
+	mockStorager.On("Update", ctx, int64(2), UpdateSuccess).Return(
+		entity.Template{
+			Id:     2,
+			Active: UpdateSuccess.Active,
+			Code:   UpdateSuccess.Code,
+			Name:   UpdateSuccess.Name,
+			Body:   UpdateSuccess.Body,
+			Title:  UpdateSuccess.Title,
+		},
+		nil,
+	)
+
+	mockStorager.On("Update", ctx, int64(3), UpdateSuccess).Return(
+		entity.Template{},
+		storager.ErrorEntityNotFound,
+	)
+
+	mockStorager.On("Update", ctx, int64(2), UpdateDublicateError).Return(
+		entity.Template{},
+		storager.ErrorDuplicate,
+	)
+
+	mockStorager.On("Delete", ctx, int64(2), UpdateDublicateError).Return(
+		nil,
+	)
+
+	mockStorager.On("Delete", ctx, int64(3), UpdateSuccess).Return(
+		storager.ErrorEntityNotFound,
+	)
 
 	return mockStorager
 }
@@ -47,4 +147,29 @@ func NewMockStorager(ctx context.Context) *MockTemplateStorager {
 func (s *MockTemplateStorager) GetByCode(ctx context.Context, code string) (entity.Template, error) {
 	args := s.Called(ctx, code)
 	return args.Get(0).(entity.Template), args.Error(1)
+}
+
+func (s *MockTemplateStorager) Get(ctx context.Context, id int64) (entity.Template, error) {
+	args := s.Called(ctx, id)
+	return args.Get(0).(entity.Template), args.Error(1)
+}
+
+func (s *MockTemplateStorager) Search(ctx context.Context, dto dto.Search) (templates []entity.Template, total int, err error) {
+	args := s.Called(ctx, dto)
+	return args.Get(0).([]entity.Template), args.Int(1), args.Error(2)
+}
+
+func (s *MockTemplateStorager) Add(ctx context.Context, dto dto.Add) (entity.Template, error) {
+	args := s.Called(ctx, dto)
+	return args.Get(0).(entity.Template), args.Error(1)
+}
+
+func (s *MockTemplateStorager) Update(ctx context.Context, id int64, dto dto.Update) (entity.Template, error) {
+	args := s.Called(ctx, id, dto)
+	return args.Get(0).(entity.Template), args.Error(1)
+}
+
+func (s *MockTemplateStorager) Delete(ctx context.Context, id int64) error {
+	args := s.Called(ctx, id)
+	return args.Error(0)
 }
